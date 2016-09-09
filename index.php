da0bfbdb0610dd07df85d4fa51decaebc0e0080e
Sam Wilson <sam@samwilson.id.au>
2016-09-09 12:30:04 +0800

    Localisation and a better front-end
    This brings the Intuition message system in, and makes a nicer
    web front-end for this tool.
    
    Refs T145104

--- index.php
+++ index.php
@@ -1,34 +1,51 @@
-<!doctype html>
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+// Optical Character Recognition.
+$ocr = new Wikisource\GoogleOcr\Ocr(__DIR__, $_GET);
+
+// Localisation.
+$i18n = new Intuition('ws-google-ocr');
+$i18n->registerDomain('ws-google-ocr', __DIR__ . '/messages');
+
+?><!doctype html>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Wikisource Google OCR</title>
+        <title><?php echo $i18n->msg('title') ?></title>
         <link rel="stylesheet" href="//tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" />
     </head>
     <body>
         <div class="container">
             <div class="page-header">
-                <h1>Wikisource Google OCR</h1>
+                <h1><?php echo $i18n->msg('title') ?></h1>
             </div>
 
-            <form action="api.php" method="get">
+            <form action="index.php" method="get">
                 <div class="form-group">
-                    <label for="image" class="form-label">Image URL:</label>
-                    <input type="text" name="image" class="form-control" />
+                    <label for="image" class="form-label"><?php echo $i18n->msg('image-url') ?></label>
+                    <input type="text" name="image" class="form-control" value="<?php echo $ocr->getImage() ?>" />
                 </div>
                 <div class="form-group">
-                    <label for="lang" class="form-label">Language code (optional):</label>
-                    <input type="text" name="lang" class="form-control" />
+                    <label for="lang" class="form-label"><?php echo $i18n->msg('language-code') ?></label>
+                    <input type="text" name="lang" class="form-control" value="<?php echo $ocr->getLang() ?>" />
                 </div>
                 <div class="form-group">
-                    <input type="submit" value="Go" class="btn btn-info" />
+                    <input type="submit" value="<?php echo $i18n->msg('submit') ?>" class="btn btn-info" />
                 </div>
             </form>
 
+            <?php if ($ocr->hasValidImage()): ?>
+            <h2><?php echo $i18n->msg('ocr-text') ?></h2>
+            <blockquote>
+                <?php echo nl2br($ocr->getText()) ?>
+            </blockquote>
+            <?php endif ?>
+
             <p class="text-muted text-right">
-                Please report any bugs or feature requests via
-                <a href="https://phabricator.wikimedia.org">Phabricator</a>
+                <?php echo $i18n->msg('issue-reporting') ?>
+                <a href="https://phabricator.wikimedia.org"><?php echo $i18n->msg('phabricator') ?></a>
             </p>
         </div>
         <script type="text/javascript" src="//tools-static.wmflabs.org/cdnjs/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
---
+++ src/Ocr.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Wikisource\GoogleOcr;
+
+use Wikisource\GoogleCloudVisionPHP\GoogleCloudVision;
+use Wikisource\GoogleCloudVisionPHP\LimitExceededException;
+
+class Ocr
+{
+
+    /** @var string The URL of the image to process. */
+    protected $image;
+
+    /** @var string The two-letter language code of the text in the image. */
+    protected $lang;
+
+    /** @var Wikisource\GoogleCloudVisionPHP\GoogleCloudVision */
+    protected $gcv;
+
+    public function __construct($baseDir, $request)
+    {
+        // Get the configuration variables.
+        require $baseDir . '/config.php';
+        if (empty($key)) {
+            throw new \Exception('API key value ($key) must be set in config.php');
+        }
+        $this->gcv = new GoogleCloudVision();
+        $this->gcv->setKey($key);
+        if (!empty($endpoint)) {
+            $this->gcv->setEndpoint($endpoint);
+        }
+
+        // Get and check the request parameters.
+        if (!empty($request['image'])) {
+            $this->image = $request['image'];
+        }
+        if (!empty($request['lang'])) {
+            $this->lang = $request['lang'];
+        }
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getLang()
+    {
+        return $this->lang;
+    }
+
+    public function hasValidImage()
+    {
+        if (!isset($this->image)) {
+            return false;
+        }
+        $uploadUrl = 'https://upload.wikimedia.org/';
+        if (substr($this->image, 0, strlen($uploadUrl)) !== $uploadUrl) {
+            return false;
+        }
+        return true;
+    }
+
+    public function getText()
+    {
+        $this->gcv->setImage($this->image);
+        $this->gcv->addFeatureOCR();
+        if ($this->getLang() !== null && $this->getLang() !== 'en') {
+            $this->gcv->setImageContext(['languageHints' => [$_REQUEST['lang']]]);
+        }
+        $response = $this->gcv->request();
+
+        // Check for errors and pass any through.
+        if (isset($response['responses'][0]['error'])) {
+            error($response['responses'][0]['error']);
+        }
+
+        // Return only the text to the user (it's not an error if there's no text).
+        $text = '';
+        if (isset($response['responses'][0]['textAnnotations'][0]['description'])) {
+            $text = $response['responses'][0]['textAnnotations'][0]['description'];
+        }
+        return $text;
+    }
+}
