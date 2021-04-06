70f93a350d99c99399b76a28cea8657d715e526f
Dayllan Maza <dmaza@wikimedia.org>
2021-04-06 12:38:45 -0400

    Guard against setup.sh being run from a diff directory

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,9 +3,9 @@
 set -e
 
 echo "Building image"
-docker build -t wikimedia-ocr .
+docker build -t wikimedia-ocr $(dirname $0)/..
 
 echo "Running composer install && npm install"
-docker run --mount type=bind,source="$(pwd)",target=/wikimedia-ocr wikimedia-ocr:latest bash ./docker/install.sh
+docker run --mount type=bind,source="$(cd "$(dirname $0)/.."; pwd)",target=/wikimedia-ocr wikimedia-ocr:latest bash ./docker/install.sh
 
 echo $'\e[1;32m'Everything looks good. Run ./docker/run.sh and an instance should be available at the default port \(8000\) $'\e[0m'
