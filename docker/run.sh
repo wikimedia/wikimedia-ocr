b1fe01d71a97a8d8303eca1a82c653f163cb1207
Dayllan Maza <dmaza@wikimedia.org>
2021-04-06 15:15:27 -0400

    Add -ti options on run.sh for better handling

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p ${DOCKER_OCR_PORT:-8000}:8000 --mount type=bind,source="$(cd "$(dirname $0)/.."; pwd)",target=/wikimedia-ocr wikimedia-ocr:latest
+docker run -ti -p ${DOCKER_OCR_PORT:-8000}:8000 --mount type=bind,source="$(cd "$(dirname $0)/.."; pwd)",target=/wikimedia-ocr wikimedia-ocr:latest
