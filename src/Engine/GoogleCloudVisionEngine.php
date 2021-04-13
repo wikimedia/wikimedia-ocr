734479a1f5e12911bce27bdcf6859781da5fd175
Sam Wilson <sam@samwilson.id.au>
2021-04-13 08:15:54 +0800

    Switch Google Cloud Vision package
    Our Wikisource fork of the old package is no longer maintained,
    and Google maintains their own now.
    
    Bug: T277129

--- src/Engine/GoogleCloudVisionEngine.php
+++ src/Engine/GoogleCloudVisionEngine.php
@@ -4,29 +4,24 @@ declare(strict_types = 1);
 namespace App\Engine;
 
 use App\Exception\OcrException;
-use ErrorException;
-use Wikisource\GoogleCloudVisionPHP\GoogleCloudVision;
-use Wikisource\GoogleCloudVisionPHP\LimitExceededException;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+use Google\Cloud\Vision\V1\ImageContext;
 
 class GoogleCloudVisionEngine extends EngineBase
 {
     /** @var string The API key. */
     protected $key;
 
-    /** @var GoogleCloudVision */
-    protected $gcv;
+    /** @var ImageAnnotatorClient */
+    protected $imageAnnotator;
 
     /**
      * GoogleCloudVisionEngine constructor.
-     * @param string $endpoint Google Cloud Vision API endpoint.
-     * @param string $key Google Cloud Vision API key.
+     * @param string $keyFile Filesystem path to the credentials JSON file.
      */
-    public function __construct(string $endpoint, string $key)
+    public function __construct(string $keyFile)
     {
-        $this->key = $key;
-        $this->gcv = new GoogleCloudVision();
-        $this->gcv->setKey($this->key);
-        $this->gcv->setEndpoint($endpoint);
+        $this->imageAnnotator = new ImageAnnotatorClient(['credentials' => $keyFile]);
     }
 
     /**
@@ -40,36 +35,17 @@ class GoogleCloudVisionEngine extends EngineBase
     {
         $this->checkImageUrl($imageUrl);
 
-        try {
-            $this->gcv->setImage($imageUrl);
-        } catch (LimitExceededException $e) {
-            throw new OcrException('limit-exceeded', [$e->getMessage()]);
-        } catch (ErrorException $e) {
-            if (false !== strpos($e->getMessage(), '404 Not Found')) {
-                // The 'image-retrieval-failed' message is the important part that is localized.
-                throw new OcrException('image-retrieval-failed', ['404 Not Found']);
-            }
-
-            // Unknown error.
-            throw $e;
-        }
-
-        $this->gcv->addFeatureDocumentTextDetection();
+        $imageContext = new ImageContext();
         if (null !== $lang && 'en' !== $lang) {
-            $this->gcv->setImageContext(['languageHints' => [$lang]]);
-        }
-        $response = $this->gcv->request();
-
-        // Check for errors and pass any through.
-        /* @phan-suppress-next-line PhanTypeMismatchDimFetch */
-        $error = $response['responses'][0]['error']['message'] ?? '';
-        if ($error) {
-            $msg = str_replace($this->key, '[KEY REDACTED]', $error);
-            throw new OcrException($msg);
+            $imageContext->setLanguageHints([$lang]);
         }
 
-        // Return only the text (it's not an error if there's no text).
-        /* @phan-suppress-next-line PhanTypeMismatchDimFetch */
-        return $response['responses'][0]['textAnnotations'][0]['description'] ?? '';
+        $response = $this->imageAnnotator->textDetection($imageUrl, ['imageContext' => $imageContext]);
+
+        if ($response->getError()) {
+            throw new OcrException('google-error', [$response->getError()->getMessage()]);
+        }
+
+        return $response->getFullTextAnnotation()->getText();
     }
 }
--- tests/Engine/EngineBaseTest.php
+++ tests/Engine/EngineBaseTest.php
@@ -14,7 +14,7 @@ class EngineBaseTest extends TestCase
      */
     public function testCheckImageUrl(): void
     {
-        $engine = new GoogleCloudVisionEngine('foo', 'bar');
+        $engine = new GoogleCloudVisionEngine(dirname(__DIR__).'/fixtures/google-account-keyfile.json');
 
         // Should not throw an exception.
         $engine->checkImageUrl('https://upload.wikimedia.org/wikipedia/commons/a/a9/Example.jpg');
