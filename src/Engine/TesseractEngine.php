f7dbff24d4dec9933808e4edb7325defdaa027ee
Sam Wilson <sam@samwilson.id.au>
2021-04-21 14:56:08 +0800

    Add OMP_THREAD_LIMIT=1 env var (#17)

--- src/Engine/TesseractEngine.php
+++ src/Engine/TesseractEngine.php
@@ -39,6 +39,11 @@ class TesseractEngine extends EngineBase
         if ($cleanLang) {
             $ocr->lang($cleanLang);
         }
+        // Env vars are passed through by the thiagoalessio/tesseract_ocr package to the tesseract command,
+        // but when they're loaded from Symfony's .env they aren't actually available (by design),
+        // so we have to load this one manually. We only process one image at a time, so don't benefit from
+        // multiple threads. See https://github.com/tesseract-ocr/tesseract/issues/898 for some more info.
+        putenv('OMP_THREAD_LIMIT=1');
         $text = $ocr->run();
         return $text;
     }
