af77b6c931051e796cf4c00f16f4093de4243a32
hmonroy <hmonroy@wikimedia.org>
2021-04-28 12:41:23 -0700

    Add language codes validation
    Add language validation input according to OCR engine.
    
    Bug: T280617

--- tests/Engine/EngineBaseTest.php
+++ tests/Engine/EngineBaseTest.php
@@ -4,23 +4,72 @@ declare(strict_types = 1);
 namespace App\Tests\Engine;
 
 use App\Engine\GoogleCloudVisionEngine;
+use App\Engine\TesseractEngine;
 use App\Exception\OcrException;
+use Krinkle\Intuition\Intuition;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use thiagoalessio\TesseractOCR\TesseractOCR;
 
 class EngineBaseTest extends TestCase
 {
+    /** @var GoogleCloudVisionEngine */
+    private $googleEngine;
+
+    /** @var TesseractEngine */
+    private $tesseractEngine;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $intuition = new Intuition();
+        $this->googleEngine = new GoogleCloudVisionEngine(
+            dirname(__DIR__).'/fixtures/google-account-keyfile.json',
+            $intuition
+        );
+
+        $tesseractOCR = $this->getMockBuilder(TesseractOCR::class)->disableOriginalConstructor()->getMock();
+        $tesseractOCR->method('availableLanguages')
+            ->will($this->returnValue(['eng', 'spa', 'tha', 'tir']));
+        $this->tesseractEngine = new TesseractEngine(new MockHttpClient(), $intuition, $tesseractOCR);
+    }
+
     /**
      * @covers EngineBase::checkImageUrl
      */
     public function testCheckImageUrl(): void
     {
-        $engine = new GoogleCloudVisionEngine(dirname(__DIR__).'/fixtures/google-account-keyfile.json');
-
         // Should not throw an exception.
-        $engine->checkImageUrl('https://upload.wikimedia.org/wikipedia/commons/a/a9/Example.jpg');
+        $this->googleEngine->checkImageUrl('https://upload.wikimedia.org/wikipedia/commons/a/a9/Example.jpg');
 
         // Should throw an exception.
         static::expectException(OcrException::class);
-        $engine->checkImageUrl('https://upload.wikimedia.org/wikipedia/commons');
+        $this->googleEngine->checkImageUrl('https://upload.wikimedia.org/wikipedia/commons');
+    }
+
+    /**
+     * @covers EngineBase::validateLangs for Google Engine
+     */
+    public function testValidateLangsGoogleEngine(): void
+    {
+        // Should not throw an exception.
+        $this->googleEngine->validateLangs(['en', 'es']);
+
+        // Should throw an exception. 'sp` is not a valid lang
+        static::expectException(OcrException::class);
+        $this->googleEngine->validateLangs(['en', 'sp']);
+    }
+
+    /**
+     * @covers EngineBase::validateLangs for Tesseract Engine
+     */
+    public function testValidateLangsTesseractEngine(): void
+    {
+        // Should not throw an exception.
+        $this->tesseractEngine->validateLangs(['eng', 'spa']);
+
+        // Should throw an exception. `en` is not a valid lang
+        static::expectException(OcrException::class);
+        $this->tesseractEngine->validateLangs(['en', 'spa']);
     }
 }
