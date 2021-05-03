619054c3c2153c91fa8cf59cca9a5096828536fe
Sam Wilson <sam@samwilson.id.au>
2021-05-04 02:05:42 +0800

    Add npm to CI (#25)
    * Add qqq descriptions.
    * Stylelink and Banana checker.
    * Check for source modifications in CI.

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/* eslint-env node */
+module.exports = function Gruntfile(grunt)
+{
+
+    grunt.loadNpmTasks('grunt-banana-checker');
+    grunt.loadNpmTasks('grunt-stylelint');
+
+    grunt.initConfig({
+        banana: {
+            all: {
+                src: 'i18n/'
+            }
+        },
+        stylelint: {
+            all: [
+                '**/*.{css,less}',
+                '!node_modules/**',
+                '!vendor/**',
+                '!var/**',
+                '!public/build/**'
+            ]
+        },
+    });
+
+    grunt.registerTask('test', ['stylelint', 'banana']);
+    grunt.registerTask('default', 'test');
+};
