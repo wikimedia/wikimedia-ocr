a799142a66ed79222c16a4be02e70660b8604844
Daimona Eaytoy <daimona.wiki@gmail.com>
2021-06-09 14:06:58 +0200

    Add a bunch of plugins and stricter config options

--- .phan/config.php
+++ .phan/config.php
@@ -18,10 +18,31 @@ return [
 
     'suppress_issue_types' => [
         'PhanUnreferencedUseNormal', // PHPCS does this already and without false positives.
+        'SecurityCheck-LikelyFalsePositive',
     ],
 
+    'enable_extended_internal_return_type_plugins' => true,
+    'generic_types_enabled' => true,
+
+    'null_casts_as_any_type' => false,
+    'scalar_implicit_cast' => false,
+    // Note: dead code detection has false positives with symfony magic methods
+
+    'redundant_condition_detection' => true,
+
     'plugins' => [
+        'UnreachableCodePlugin',
+        'PregRegexCheckerPlugin',
+        'UnusedSuppressionPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'RedundantAssignmentPlugin',
+        'StrictLiteralComparisonPlugin',
+        'DollarDollarPlugin',
+        'LoopVariableReusePlugin',
+        'StrictComparisonPlugin',
+        'SimplifyExpressionPlugin',
         'vendor/drenso/phan-extensions/Plugin/Annotation/SymfonyAnnotationPlugin.php',
-        'vendor/mediawiki/phan-taint-check-plugin/GenericSecurityCheckPlugin.php'
+        'vendor/mediawiki/phan-taint-check-plugin/GenericSecurityCheckPlugin.php',
     ],
 ];
