7729aec022cc208db67bf8004dff4851ec8c6dbf
Daimona <38216014+Daimona@users.noreply.github.com>
2021-06-10 05:48:04 +0200

    Strengthen the configuration options for phan
    * Add phan-taint-check-plugin to spot possible security issues (although it still cannot understand templates)
    * Pass --long-progress-bar explicitly to make sure the progress bar it's always shown
    * Add a few config options for stricter analysis. This is a good set of options, and doesn't cause any new issue to be reported
    * Install the php-ast extension in the github CI to run phan faster
    
    Note that we need to downgrade phan due to the strict dependency that taint-check has.
    
    Bug: T284571

--- .phan/config.php
+++ .phan/config.php
@@ -2,61 +2,47 @@
 
 declare(strict_types = 1);
 
-/**
- * This configuration will be read and overlaid on top of the
- * default configuration. Command-line arguments will be applied
- * after this file is read.
- */
 return [
-    // Supported values: `'5.6'`, `'7.0'`, `'7.1'`, `'7.2'`, `'7.3'`,
-    // `'7.4'`, `null`.
-    // If this is set to `null`,
-    // then Phan assumes the PHP version which is closest to the minor version
-    // of the php executable used to execute Phan.
-    //
-    // Note that the **only** effect of choosing `'5.6'` is to infer
-    // that functions removed in php 7.0 exist.
-    // (See `backward_compatibility_checks` for additional options)
     'target_php_version' => null,
 
-    // A list of directories that should be parsed for class and
-    // method information. After excluding the directories
-    // defined in exclude_analysis_directory_list, the remaining
-    // files will be statically analyzed for errors.
-    //
-    // Thus, both first-party and third-party code being used by
-    // your application should be included in this list.
     'directory_list' => [
         'src',
         'vendor',
     ],
 
-    // A regex used to match every file name that you want to
-    // exclude from parsing. Actual value will exclude every
-    // "test", "tests", "Test" and "Tests" folders found in
-    // "vendor/" directory.
     'exclude_file_regex' => '@^vendor/.*/(tests?|Tests?)/@',
 
-    // A directory list that defines files that will be excluded
-    // from static analysis, but whose class and method
-    // information should be included.
-    //
-    // Generally, you'll want to include the directories for
-    // third-party code (such as "vendor/") in this list.
-    //
-    // n.b.: If you'd like to parse but not analyze 3rd
-    //       party code, directories containing that code
-    //       should be added to both the `directory_list`
-    //       and `exclude_analysis_directory_list` arrays.
     'exclude_analysis_directory_list' => [
         'vendor/',
     ],
 
     'suppress_issue_types' => [
         'PhanUnreferencedUseNormal', // PHPCS does this already and without false positives.
+        'SecurityCheck-LikelyFalsePositive',
     ],
 
+    'enable_extended_internal_return_type_plugins' => true,
+    'generic_types_enabled' => true,
+
+    'null_casts_as_any_type' => false,
+    'scalar_implicit_cast' => false,
+    // Note: dead code detection has false positives with symfony magic methods
+
+    'redundant_condition_detection' => true,
+
     'plugins' => [
+        'UnreachableCodePlugin',
+        'PregRegexCheckerPlugin',
+        'UnusedSuppressionPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'RedundantAssignmentPlugin',
+        'StrictLiteralComparisonPlugin',
+        'DollarDollarPlugin',
+        'LoopVariableReusePlugin',
+        'StrictComparisonPlugin',
+        'SimplifyExpressionPlugin',
         'vendor/drenso/phan-extensions/Plugin/Annotation/SymfonyAnnotationPlugin.php',
+        'vendor/mediawiki/phan-taint-check-plugin/GenericSecurityCheckPlugin.php',
     ],
 ];
