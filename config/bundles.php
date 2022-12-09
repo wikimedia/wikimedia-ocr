f88a032b30b2ec6f5f2a26d24cd0760d55f22145
Sam Wilson <sam@samwilson.id.au>
2022-12-09 10:44:09 +0800

    Remove Flex and update PHP dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Wikimedia\ToolforgeBundle\ToolforgeBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
