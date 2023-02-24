c838b722b101fdd7781f6c46bb857efc236094e6
Parthiv <pmenon-ctr@wikimedia.org>
2023-02-24 09:27:58 +0530

    Remove unused import from TranskribusAuthCommand

--- src/Command/TranskribusAuthCommand.php
+++ src/Command/TranskribusAuthCommand.php
@@ -8,7 +8,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TranskribusAuthCommand extends Command
 {
